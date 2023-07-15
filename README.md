83d90556ded499921e35399b4cdd85319a3dfa10
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2023-07-15 20:03:49 +0300

    Добавляет адрес API, убирает лишнее

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # movies-explorer-api
+
+## Ссылки на проект
+
+IP 158.160.74.4
+
+API URL:
+https://api.mirvada.nomoredomains.xyz
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,5 @@
 const MONGO_DB = process.env.MONGO_DB || 'mongodb://127.0.0.1:27017/bitfilmsdb';
 const PORT = process.env.PORT || 3000;
-// const { JWT } = process.env.JWT;
 const SECRET = process.env.NODE_ENV === 'production' ? process.env.JWT : 'super-strong-key';
 
 module.exports = {
